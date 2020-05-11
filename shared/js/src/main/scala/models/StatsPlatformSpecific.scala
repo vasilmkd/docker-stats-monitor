0c54c69c784ec1ac395310e6192736978cdac789
Vasil Vasilev <vasilv@protonmail.com>
2020-05-11 21:11:38 +0200

    Add model parsing tests

--- shared/js/src/main/scala/models/StatsPlatformSpecific.scala
+++ shared/js/src/main/scala/models/StatsPlatformSpecific.scala
@@ -2,9 +2,13 @@ package models
 
 import scala.scalajs.js
 
+import cats.{ MonadError, Traverse }
+import cats.implicits._
+
 private[models] trait StatsPlatformSpecific {
-  def apply(obj: js.Dynamic): Stats = {
-    val data = obj.selectDynamic("data").asInstanceOf[js.Array[js.Dynamic]].map(ContainerData(_)).toSet
-    Stats(data)
-  }
+  def parse[F[_]: MonadError[*[_], Throwable]](obj: js.Dynamic): F[Stats] =
+    MonadError[F, Throwable].catchNonFatal {
+      val data = obj.data.asInstanceOf[js.UndefOr[js.Array[js.Dynamic]]].get.map(ContainerData.parse[F](_)).toList
+      Traverse[List].sequence(data).map(cds => Stats(cds.toSet))
+    }.flatten
 }
