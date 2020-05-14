0baa62d34b8d27610d47293e24718eff42bd328b
Vasil Vasilev <vasilv@protonmail.com>
2020-05-14 22:19:28 +0200

    Factor out stream source and test stream

--- server/src/main/scala/server/DockerStats.scala
+++ server/src/main/scala/server/DockerStats.scala
@@ -2,36 +2,12 @@ package server
 
 import java.io.InputStream
 
-import scala.concurrent.duration._
-
-import cats.Functor
-import cats.effect.{ Blocker, ContextShift, Sync, Timer }
-import fs2.{ text, Stream }
-import fs2.io._
-
-import models._
+import cats.effect.Sync
 
 object DockerStats {
 
-  def stream[F[_]: Sync: ContextShift: Timer](blocker: Blocker): Stream[F, Stats] =
-    ticker(statsStream(blocker))
-
-  private def ticker[F[_]: Functor: Timer, A](stream: Stream[F, A]): Stream[F, A] =
-    (Stream.emit(Duration.Zero) ++ Stream.awakeEvery[F](5.seconds))
-      .map(_ => stream)
-      .flatten
-
-  private def statsStream[F[_]: Sync: ContextShift](blocker: Blocker): Stream[F, Stats] =
-    readInputStream(processInputStream(statsBuilder), 8192, blocker)
-      .through(text.utf8Decode)
-      .through(text.lines)
-      .filter(_.nonEmpty)
-      .drop(1)
-      .evalMap(ContainerData.parseCSV(_))
-      .fold(Stats(Set.empty))((s, cd) => Stats(s.data + cd))
-
-  private def processInputStream[F[_]: Sync](pb: ProcessBuilder): F[InputStream] =
-    Sync[F].delay(pb.start().getInputStream())
+  def input[F[_]: Sync]: F[InputStream] =
+    Sync[F].delay(statsBuilder.start().getInputStream())
 
   private val statsBuilder = new ProcessBuilder()
     .command(
---
+++ server/src/main/scala/server/StatsStream.scala
@@ -0,0 +1,33 @@
+package server
+
+import java.io.InputStream
+
+import scala.concurrent.duration._
+
+import cats.Functor
+import cats.effect.{ Blocker, ContextShift, Sync, Timer }
+import fs2.{ text, Stream }
+import fs2.io._
+
+import models._
+
+object StatsStream {
+
+  def stream[F[_]: Sync: ContextShift: Timer](fis: F[InputStream], blocker: Blocker): Stream[F, Stats] =
+    ticker(statsStream(fis, blocker))
+
+  private def ticker[F[_]: Functor: Timer, A](stream: Stream[F, A]): Stream[F, A] =
+    (Stream.emit(Duration.Zero) ++ Stream.awakeEvery[F](5.seconds))
+      .as(stream)
+      .flatten
+
+  private def statsStream[F[_]: Sync: ContextShift](fis: F[InputStream], blocker: Blocker): Stream[F, Stats] =
+    readInputStream(fis, 8192, blocker)
+      .through(text.utf8Decode)
+      .through(text.lines)
+      .filter(_.nonEmpty)
+      .drop(1)
+      .evalMap(ContainerData.parseCSV(_))
+      .fold(Set.empty[ContainerData])(_ + _)
+      .map(Stats(_))
+}
