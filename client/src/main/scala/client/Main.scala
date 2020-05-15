af6ae3310fbaacef6eeb250d60df94a3e0e5fd9f
Vasil Vasilev <vasilv@protonmail.com>
2020-05-15 03:30:38 +0200

    Prepare client application for testing by making it a pipe

--- client/src/main/scala/client/Main.scala
+++ client/src/main/scala/client/Main.scala
@@ -1,9 +1,26 @@
 package client
 
 import cats.effect.{ ExitCode, IO, IOApp }
+import fs2.Stream
+import fs2.concurrent.Queue
+import org.scalajs.dom._
 
-object CatsMain extends IOApp {
+object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] =
-    new Client[IO].run.as(ExitCode.Success)
+    Stream
+      .eval {
+        for {
+          queue <- Queue.unbounded[IO, MessageEvent]
+          ws    <- IO(new WebSocket("ws://localhost:8080/ws"))
+          _ <- IO {
+                ws.onmessage = e => queue.enqueue1(e).unsafeRunSync()
+              }
+        } yield queue
+      }
+      .flatMap(queue => queue.dequeue)
+      .through(new Client[IO].run)
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
