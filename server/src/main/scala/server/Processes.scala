67c414d0f512a58072061fbca0c07fff1b07f771
Vasil Vasilev <vasilv@protonmail.com>
2020-05-16 16:40:53 +0200

    Add Processes model

---
+++ server/src/main/scala/server/Processes.scala
@@ -0,0 +1,32 @@
+package server
+
+import cats.{ Applicative, MonadError }
+import cats.implicits._
+
+final case class Processes(
+  id: String,
+  image: String,
+  created: String,
+  ports: String,
+  status: String,
+  size: String
+)
+
+object Processes {
+  def parseCSV[F[_]: MonadError[*[_], Throwable]](line: String): F[Processes] =
+    for {
+      parts     <- splitLine[F](line)
+      processes <- parseParts[F](parts)
+    } yield processes
+
+  private def splitLine[F[_]: MonadError[*[_], Throwable]](line: String): F[Array[String]] =
+    for {
+      parts <- Applicative[F].pure(line.split("\t"))
+      _ <- MonadError[F, Throwable]
+            .raiseError(new IllegalStateException(s"Invalid docker ps data csv: $line"))
+            .whenA(parts.length < 6)
+    } yield parts
+
+  private def parseParts[F[_]: MonadError[*[_], Throwable]](parts: Array[String]): F[Processes] =
+    MonadError[F, Throwable].catchNonFatal(Processes(parts(0), parts(1), parts(2), parts(3), parts(4), parts(5)))
+}
