fcd3dc70818260cf1164dfddc922f54ebc284704
Vasil Vasilev <vasilv@protonmail.com>
2020-05-16 13:03:28 +0200

    Cleanup client main

--- client/src/main/scala/client/Main.scala
+++ client/src/main/scala/client/Main.scala
@@ -1,14 +1,8 @@
 package client
 
 import cats.effect.{ ExitCode, IO, IOApp }
-import fs2.Stream
-import fs2.concurrent.Queue
-import io.circe.generic.auto._
-import io.circe.parser._
-import org.scalajs.dom._
 
 import client.impl._
-import models._
 
 object Main extends IOApp {
 
@@ -16,24 +10,10 @@ object Main extends IOApp {
   implicit private val ioCharting = ChartingImpl[IO]
 
   def run(args: List[String]): IO[ExitCode] =
-    Stream
-      .eval {
-        for {
-          queue <- Queue.unbounded[IO, MessageEvent]
-          ws    <- IO(new WebSocket("ws://localhost:8080/ws"))
-          _ <- IO {
-                ws.onmessage = e => queue.enqueue1(e).unsafeRunSync()
-              }
-        } yield queue
-      }
-      .flatMap(queue => queue.dequeue)
-      .map(_.data.toString)
-      .evalMap(decodeStats)
+    WebsocketStream
+      .stream[IO]
       .through(new Client[IO].run)
       .compile
       .drain
       .as(ExitCode.Success)
-
-  private def decodeStats(json: String): IO[Stats] =
-    IO.fromEither(decode[Stats](json))
 }
---
+++ client/src/main/scala/client/WebsocketStream.scala
@@ -0,0 +1,30 @@
+package client
+
+import cats.effect.{ ConcurrentEffect, Effect, IO, Sync }
+import cats.implicits._
+import fs2.Stream
+import fs2.concurrent.Queue
+import io.circe.generic.auto._
+import io.circe.parser._
+import org.scalajs.dom._
+
+import models._
+
+object WebsocketStream {
+
+  def stream[F[_]: ConcurrentEffect]: Stream[F, Stats] =
+    Stream
+      .eval {
+        for {
+          queue <- Queue.unbounded[F, MessageEvent]
+          ws    <- Sync[F].delay(new WebSocket("ws://localhost:8080/ws"))
+          _     <- Sync[F].delay(ws.onmessage = e => Effect[F].runAsync(queue.enqueue1(e))(_ => IO.unit).unsafeRunSync())
+        } yield queue
+      }
+      .flatMap(_.dequeue)
+      .map(_.data.toString)
+      .evalMap(decodeStats[F])
+
+  private def decodeStats[F[_]: Sync](json: String): F[Stats] =
+    Sync[F].fromEither(decode[Stats](json))
+}
