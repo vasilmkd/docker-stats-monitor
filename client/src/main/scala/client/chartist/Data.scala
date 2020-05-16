9ce39bdb3b1efe49f39f6e389dc5197eb03f3bcb
Vasil Vasilev <vasilv@protonmail.com>
2020-05-16 02:27:26 +0200

    Start from zero when creating charts

--- client/src/main/scala/client/chartist/Data.scala
+++ client/src/main/scala/client/chartist/Data.scala
@@ -3,9 +3,9 @@ package client.chartist
 import scala.scalajs.js
 
 class Data(initial: Double) extends js.Object {
-  private var counter                    = 1
-  val labels: js.Array[Int]              = js.Array(0)
-  val series: js.Array[js.Array[Double]] = js.Array(js.Array(initial))
+  private var counter                    = 2
+  val labels: js.Array[Int]              = js.Array(0, 1)
+  val series: js.Array[js.Array[Double]] = js.Array(js.Array(0.0, initial))
 
   def add(v: Double): Unit = {
     labels.push(counter)
