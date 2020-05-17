9bd2985cc9a9c88540d06ef856162947dc32ac9f
Vasil Vasilev <vasilv@protonmail.com>
2020-05-17 14:08:02 +0200

    Zip stats and ps streams together

--- shared/shared/src/main/scala/model/ContainerData.scala
+++ shared/shared/src/main/scala/model/ContainerData.scala
@@ -3,10 +3,15 @@ package model
 final case class ContainerData(
   id: String,
   name: String,
+  image: String,
+  runningFor: String,
+  status: String,
   cpuPercentage: Double,
   memUsage: String,
   memPercentage: Double,
   netIO: String,
   blockIO: String,
-  pids: Int
+  pids: Int,
+  size: String,
+  ports: String
 )
