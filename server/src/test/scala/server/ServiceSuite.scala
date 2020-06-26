6da034ee2979eae6a3b88014fc6e8182b620e8e2
Vasil Vasilev <vasilv@protonmail.com>
2020-06-26 17:18:26 +0200

    Upgrade http4s version

--- server/src/test/scala/server/ServiceSuite.scala
+++ server/src/test/scala/server/ServiceSuite.scala
@@ -31,7 +31,7 @@ class ServiceSuite extends FunSuite {
           res   <- new Service[IO](blocker, topic).routes.orNotFound.run(req)
           _     <- IO(assertEquals(res.status, Status.Ok))
           _     <- IO(assertEquals(res.httpVersion, HttpVersion.`HTTP/1.1`))
-          body  <- res.bodyAsText.compile.string
+          body  <- res.bodyText.compile.string
           fis    = IO(new FileInputStream(new File("static/html/index.html")))
           file  <- readInputStream[IO](fis, 8192, blocker).through(text.utf8Decode).compile.string
         } yield assertEquals(body, file)
@@ -46,7 +46,7 @@ class ServiceSuite extends FunSuite {
         for {
           topic <- Topic[IO, DockerData](DockerData.empty)
           res   <- new Service[IO](blocker, topic).routes.orNotFound.run(req)
-          body  <- res.bodyAsText.compile.string
+          body  <- res.bodyText.compile.string
         } yield assertEquals(body, "This is a WebSocket route.")
       }
       .unsafeRunSync()
